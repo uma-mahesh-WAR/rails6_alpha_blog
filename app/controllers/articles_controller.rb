dd8fb537dd0ffea939b7c2881be1f0be7de50bb2
uma-mahesh-WAR <maheshreddyraya@gmail.com>
2022-01-05 20:54:34 +0530

    Clean up and use partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,20 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: %i[show edit update destroy]
+
 	def index
 		@articles = Article.all
 	end
 
-	def show
-		@article = Article.find(params[:id])
-	end
+	def show; end
 
 	def new
 		@article = Article.new
 	end
 
-	def edit
-		@article = Article.find(params[:id])
-	end
+	def edit; end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = 'Article created successfully'
 			redirect_to article_path(@article)
@@ -26,8 +24,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params)
 			flash[:notice] = 'Article updated successfully'
 			redirect_to @article
 		else
@@ -36,8 +33,17 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
