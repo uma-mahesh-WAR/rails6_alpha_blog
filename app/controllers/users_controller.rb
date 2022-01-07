ca431a7796585cff66e4b050134cef51f704a71a
uma-mahesh-WAR <maheshreddyraya@gmail.com>
2022-01-07 10:32:52 +0530

    Add user signup and edit user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = 'Profile updated successfully'
+      redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "User SignedUp Successfully, Welcome to Alpha Blog #{@user.username}"
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+end
